9345b2ffbbfb337a209a34fa1cce01f8a87e6eac
Vendula Zbořilová <vendulazborilova@gmail.com>
2025-05-24 11:48:59 +0200

    upravy od Toma

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import RoomList from './components/RoomList';
+
+function App() {
+  return (
+    <div className="App">
+      <RoomList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/RoomList.jsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+
+
+const RoomList = () => {
+  const [rooms, setRooms] = useState([]); // do rooms se uloží data z APIimport React from 'react';
+
+    useEffect(() => {
+    fetch('http://localhost:4000/api/rooms')
+      .then((response) => response.json())
+      .then((data) => {
+        setRooms(data); // sem uložíš výsledek z API
+      });
+  }, []); // prázdné pole = provede se jen jednou při načtení
+
+    return (
+    <div>
+      <h2>Naše pokoje</h2>
+      <ul>
+        {rooms.map((room) => (
+          <li key={room.id}>
+            <h3>{room.title}</h3>
+            <img src={room.image} alt={room.title} width="250" />
+            <p>{room.description}</p>
+            <p><strong>Cena:</strong> {room.price} Kč / noc</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default RoomList;
+
